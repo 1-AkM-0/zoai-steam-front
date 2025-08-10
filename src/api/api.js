f0f2be49a9c8391659cc04aff556522277bee025
1-AkM-0 <thepe880@gmail.com>
2025-08-10 17:23:14 -0300

    feat: Add backend address

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://zoai-steam.onrender.com/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
